e06ac44b748e835c89b448c8f9b9f5515ec04af4
Shuai Kyle Zheng <kylezheng04@gmail.com>
2016-10-22 14:13:36 +0100

    update crfasrnn_demo

--- python-scripts/crfasrnn_demo.py
+++ python-scripts/crfasrnn_demo.py
@@ -62,37 +62,48 @@ def getpallete(num_cls):
                         lab >>= 3
         return pallete
 
+def crfasrnn_segmenter(model_file, pretrained_file, gpudevice, inputs):
+    if gpudevice >= 0:
+        #Do you have GPU device? NO GPU is -1!
+        has_gpu = 1
+        #which gpu device is available?
+        gpu_device=gpudevice#assume the first gpu device is available, e.g. Titan X
+    else:
+        has_gpu = 0
+    if has_gpu==1:
+        caffe.set_device(gpu_device)
+        caffe.set_mode_gpu()
+    else:
+        caffe.set_mode_cpu()
 
+
+    net = caffe.Net(model_file, pretrained_file, caffe.TEST)
+
+    input_ = np.zeros((len(inputs),
+        500, 500, inputs[0].shape[2]),
+        dtype=np.float32)
+    for ix, in_ in enumerate(inputs):
+        input_[ix] = in_
+
+    # Segment
+    caffe_in = np.zeros(np.array(input_.shape)[[0,3,1,2]],
+                        dtype=np.float32)
+    for ix, in_ in enumerate(input_):
+        caffe_in[ix] = in_.transpose((2, 0, 1))
+    tic()
+    out = net.forward_all(**{net.inputs[0]: caffe_in})
+    toc()
+    predictions = out[net.outputs[0]]
+
+    return predictions[0].argmax(axis=0).astype(np.uint8)
 
 def run_crfasrnn(inputfile, outputfile, gpudevice):
     MODEL_FILE = 'TVG_CRFRNN_new_deploy.prototxt'
     PRETRAINED = 'TVG_CRFRNN_COCO_VOC.caffemodel'
     IMAGE_FILE = inputfile
 
-    if gpudevice > 0:
-        #Do you have GPU device?
-        has_gpu = 1
-        #which gpu device is available?
-        gpu_device=gpudevice#assume the first gpu device is available, e.g. Titan X
-    else:
-        has_gpu = 0
-
-
-    if has_gpu==1:
-        caffe.set_device(gpu_device)
-        caffe.set_mode_gpu()
-        tic()
-        net = caffe.Segmenter(MODEL_FILE, PRETRAINED,True)
-        toc()
-    else:
-        caffe.set_mode_cpu()
-        tic()
-        net = caffe.Segmenter(MODEL_FILE, PRETRAINED,False)
-        toc()
-
-
     input_image = 255 * caffe.io.load_image(IMAGE_FILE)
-
+    input_image = resizeImage(input_image)
 
     width = input_image.shape[0]
     height = input_image.shape[1]
@@ -117,34 +128,48 @@ def run_crfasrnn(inputfile, outputfile, gpudevice):
     pad_w = 500 - cur_w
     im = np.pad(im, pad_width=((0, pad_h), (0, pad_w), (0, 0)), mode = 'constant', constant_values = 0)
     # Get predictions
-    segmentation = net.predict([im])
+    #segmentation = net.predict([im])
+    segmentation  = crfasrnn_segmenter(MODEL_FILE,PRETRAINED,gpudevice,[im])
+
     segmentation2 = segmentation[0:cur_h, 0:cur_w]
     output_im = PILImage.fromarray(segmentation2)
     output_im.putpalette(pallete)
+    output_im.save(outputfile)
 
-    plt.imshow(output_im)
-    plt.savefig(outputfile)
 
+def resizeImage(image):
+        width = image.shape[0]
+        height = image.shape[1]
+        maxDim = max(width,height)
+        if maxDim>500:
+            if height>width:
+                ratio = float(500.0/height)
+            else:
+                ratio = float(500.0/width)
+            image = PILImage.fromarray(np.uint8(image))
+            image = image.resize((int(height*ratio), int(width*ratio)),resample=PILImage.BILINEAR)
+            image = np.array(image)
+        return image
 
 def main(argv):
    inputfile = 'input.jpg'
    outputfile = 'output.png'
-   gpu_device = 0
+   gpu_device = 0 # use -1 to run only on CPU, use 0-3[7] to run on GPU
    try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+      opts, args = getopt.getopt(argv,'hi:o:g:',["ifile=","ofile=","gpu="])
    except getopt.GetoptError:
-      print 'crfasrnn_demo.py -i <inputfile> -o <outputfile> -gpu <gpu_device>'
+      print 'crfasrnn_demo.py -i <inputfile> -o <outputfile> -g <gpu_device>'
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print 'crfasrnn_demo.py -i <inputfile> -o <outputfile> -gpu <gpu_device>'
+         print 'crfasrnn_demo.py -i <inputfile> -o <outputfile> -g <gpu_device>'
          sys.exit()
-      elif opt in ("-i", "--ifile"):
+      elif opt in ("-i", "ifile"):
          inputfile = arg
-      elif opt in ("-o", "--ofile"):
+      elif opt in ("-o", "ofile"):
          outputfile = arg
-      elif opt in ("-gpu", "--gpudevice"):
-         gpu_device = arg
+      elif opt in ("-g", "gpudevice"):
+         gpu_device = int(arg)
    print 'Input file is "', inputfile
    print 'Output file is "', outputfile
    print 'GPU_DEVICE is "', gpu_device
