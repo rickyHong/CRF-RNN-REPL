93bfd7b3fca49ac87a33eb8f2a3eb888b63f2332
Sadeep Jayasumana <sadeep.jay@gmail.com>
2017-03-31 21:28:13 +0100

    Improved the Python code used for running the network. No changes in the functionality

--- python-scripts/crfasrnn_demo.py
+++ python-scripts/crfasrnn_demo.py
@@ -16,6 +16,7 @@ For more information about CRF-RNN, please vist the project website http://crfas
 import sys
 import time
 import getopt
+import os
 import numpy as np
 from PIL import Image as PILImage
 
@@ -23,17 +24,20 @@ from PIL import Image as PILImage
 _CAFFE_ROOT = "../caffe/"
 
 # Model definition and model file paths
-_MODEL_DEF_FILE = 'TVG_CRFRNN_new_deploy.prototxt'  # Contains the network definition.
-_MODEL_FILE = 'TVG_CRFRNN_COCO_VOC.caffemodel'  # Contains the trained weights. Must be downloaded separately.
+_MODEL_DEF_FILE = "TVG_CRFRNN_new_deploy.prototxt"  # Contains the network definition
+_MODEL_FILE = "TVG_CRFRNN_COCO_VOC.caffemodel"  # Contains the trained weights. Download from http://goo.gl/j7PrPZ
 
 sys.path.insert(0, _CAFFE_ROOT + "python")
 import caffe
+
+_MAX_DIM = 500
+
 
 def get_palette(num_cls):
     """ Returns the color map for visualizing the segmentation mask.
 
     Args:
-        num_cls: Number of classes in the dataset.
+        num_cls: Number of classes
 
     Returns:
         The color map
@@ -56,18 +60,22 @@ def get_palette(num_cls):
     return palette
 
 
-def crfasrnn_segmenter(model_def_file, model_file, gpu_device, inputs):
+def crfrnn_segmenter(model_def_file, model_file, gpu_device, inputs):
     """ Returns the segmentation of the given image.
 
     Args:
-        model_def_file: File path of the Caffe model definition prototxt file.
-        model_file: File path of the trained model file. (contains trained weights)
+        model_def_file: File path of the Caffe model definition prototxt file
+        model_file: File path of the trained model file (contains trained weights)
         gpu_device: ID of the GPU device. If using the CPU, set this to -1
-        inputs: The image input
+        inputs: List of images to be segmented 
 
     Returns:
         The segmented image
     """
+    
+    assert os.path.isfile(model_def_file), "File {} is missing".format(model_def_file)
+    assert os.path.isfile(model_file), ("File {} is missing. Please download it using "
+                                        "./download_trained_model.sh").format(model_file)
 
     if gpu_device >= 0:
         caffe.set_device(gpu_device)
@@ -77,20 +85,19 @@ def crfasrnn_segmenter(model_def_file, model_file, gpu_device, inputs):
 
     net = caffe.Net(model_def_file, model_file, caffe.TEST)
 
-    input_ = np.zeros((len(inputs), 500, 500, inputs[0].shape[2]),
-                      dtype=np.float32)
+    num_images = len(inputs)
+    num_channels = inputs[0].shape[2]
+    assert num_channels == 3, "Unexpected channel count. A 3-channel RGB image is exptected."
+    
+    caffe_in = np.zeros((num_images, num_channels, _MAX_DIM, _MAX_DIM), dtype=np.float32)
     for ix, in_ in enumerate(inputs):
-        input_[ix] = in_
-
-    # Segment
-    caffe_in = np.zeros(np.array(input_.shape)[[0, 3, 1, 2]],
-                        dtype=np.float32)
-    for ix, in_ in enumerate(input_):
         caffe_in[ix] = in_.transpose((2, 0, 1))
+
     start_time = time.time()
     out = net.forward_all(**{net.inputs[0]: caffe_in})
     end_time = time.time()
-    print("Time taken to run the network {:.4f} seconds".format(end_time - start_time))
+
+    print("Time taken to run the network: {:.4f} seconds".format(end_time - start_time))
     predictions = out[net.outputs[0]]
 
     return predictions[0].argmax(axis=0).astype(np.uint8)
@@ -103,7 +110,6 @@ def run_crfrnn(input_file, output_file, gpu_device):
         input_file: Input RGB image file (e.g. in JPEG format)
         output_file: Path to save the resulting segmentation in PNG format
         gpu_device: ID of the GPU device. If using the CPU, set this to -1
-
     """
 
     input_image = 255 * caffe.io.load_image(input_file)
@@ -124,12 +130,12 @@ def run_crfrnn(input_file, output_file, gpu_device):
 
     # Pad as necessary
     cur_h, cur_w, cur_c = im.shape
-    pad_h = 500 - cur_h
-    pad_w = 500 - cur_w
+    pad_h = _MAX_DIM - cur_h
+    pad_w = _MAX_DIM - cur_w
     im = np.pad(im, pad_width=((0, pad_h), (0, pad_w), (0, 0)), mode='constant', constant_values=0)
 
     # Get predictions
-    segmentation = crfasrnn_segmenter(_MODEL_DEF_FILE, _MODEL_FILE, gpu_device, [im])
+    segmentation = crfrnn_segmenter(_MODEL_DEF_FILE, _MODEL_FILE, gpu_device, [im])
     segmentation = segmentation[0:cur_h, 0:cur_w]
 
     output_im = PILImage.fromarray(segmentation)
@@ -151,14 +157,15 @@ def resize_image(image):
     width, height = image.shape[0], image.shape[1]
     max_dim = max(width, height)
 
-    if max_dim > 500:
+    if max_dim > _MAX_DIM:
         if height > width:
-            ratio = float(500.0 / height)
+            ratio = float(_MAX_DIM) / height
         else:
-            ratio = float(500.0 / width)
+            ratio = float(_MAX_DIM) / width
         image = PILImage.fromarray(np.uint8(image))
         image = image.resize((int(height * ratio), int(width * ratio)), resample=PILImage.BILINEAR)
         image = np.array(image)
+
     return image
 
 
@@ -188,7 +195,7 @@ def main(argv):
     print("Input file: {}".format(input_file))
     print("Output file: {}".format(output_file))
     if gpu_device >= 0:
-        print("GPU device id: {}".format(gpu_device))
+        print("GPU device ID: {}".format(gpu_device))
     else:
         print("Using the CPU (set parameters appropriately to use the GPU)")
     run_crfrnn(input_file, output_file, gpu_device)
