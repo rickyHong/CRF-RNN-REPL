3b9c6b61e5416d2d38b5c6cdf6f8dcc02fafcc43
Shuai Zheng <kylezheng04@gmail.com>
2017-05-25 15:32:29 -0700

    Update crfasrnn_demo.py
    Fix a bug. PIL reads image in the form of RGB, while cv2 reads image in the form of BGR.

--- python-scripts/crfasrnn_demo.py
+++ python-scripts/crfasrnn_demo.py
@@ -119,8 +119,8 @@ def run_crfrnn(input_file, output_file, gpu_device):
     image = np.array(image)
 
     palette = get_palette(256)
-
-    mean_vec = np.array([103.939, 116.779, 123.68], dtype=np.float32)
+    #PIL reads image in the form of RGB, while cv2 reads image in the form of BGR, mean_vec = [R,G,B] 
+    mean_vec = np.array([123.68, 116.779, 103.939], dtype=np.float32)
     mean_vec = mean_vec.reshape(1, 1, 3)
 
     # Rearrange channels to form BGR
